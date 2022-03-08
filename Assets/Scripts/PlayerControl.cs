b5d58cb70e8e7ac1b01c52ca8b7d384f5bb5322f
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-03-08 13:14:05 +0900

    プレイヤーの移動を作っている最中

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,6 +11,7 @@ public class PlayerControl : MonoBehaviour
     //ÉvÉåÉCÉÑÅ[ÇÃìÆÇ´
     [SerializeField] float _movePower = 3f;
     [SerializeField] float _jumpPower = 3f;
+    [SerializeField] float _rotateSpeed = 3f;
     int _jumpCount = 0;
     int _maxJump = 1;
 
@@ -27,6 +28,20 @@ public class PlayerControl : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");
         float h = Input.GetAxisRaw("Horizontal");
 
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
 
+        if(dir == Vector3.zero)
+        {
+            _rb.velocity = new Vector3(0, _rb.velocity.y, 0);
+        }
+        else
+        {
+            Quaternion rotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime * _rotateSpeed);
+        }
+
+        Vector3 vel = dir.normalized * _movePower;
+        vel.y = _rb.velocity.y;
+        _rb.velocity = vel;
     }
 }
