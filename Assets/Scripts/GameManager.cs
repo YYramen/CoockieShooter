a74bd94da51fbaf7a8d45688713e5ca34b153c6d
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-05-17 15:52:20 +0900

    銃の作成、銃はクロスヘア(照準)の方向に向く

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+/// <summary>
+/// ゲームを管理するスクリプト、適当なオブジェクトにアタッチして設定すれば動く
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>照準のUI(Image)</summary>
+    [SerializeField] Image _crosshairImage;
+    /// <summary>銃のオブジェクト</summary>
+    [SerializeField] GameObject _gunObject;
+    /// <summary> 銃から飛ばされるRayの距離 </summary>
+    [SerializeField] float _rayRange = 100f;
+    /// <summary>ゲームスタート時に呼び出す処理</summary>
+    [SerializeField] UnityEngine.Events.UnityEvent _onGameStart = null;
+
+    public void StartGame() //ゲームプレイを初期化する
+    {
+
+
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        // 照準の処理
+        _crosshairImage.rectTransform.position = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _rayRange))
+        {
+            _gunObject.transform.LookAt(hit.point);    // 銃の方向を変えている
+        }
+    }
+
+    void AddCoin()  //敵を倒した時にコインをプレイヤーに与える
+    {
+
+    }
+
+    private void OnApplicationQuit()
     {
         
     }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,13 +8,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
-    //プレイヤーの動き
-    [SerializeField] float _movePower = 3f;
-    [SerializeField] float _jumpPower = 3f;
-    [SerializeField] float _rotateSpeed = 3f;
-    int _jumpCount = 0;
-    int _maxJump = 1;
-
     Rigidbody _rb;
 
     void Start()
@@ -24,24 +17,6 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
-        // 入力を受け付けて動かす
-        float v = Input.GetAxisRaw("Vertical");
-        float h = Input.GetAxisRaw("Horizontal");
-
-        Vector3 dir = Vector3.forward * v + Vector3.right * h;
-
-        if(dir == Vector3.zero)
-        {
-            _rb.velocity = new Vector3(0, _rb.velocity.y, 0);
-        }
-        else
-        {
-            Quaternion rotation = Quaternion.LookRotation(dir);
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, Time.deltaTime * _rotateSpeed);
-        }
-
-        Vector3 vel = dir.normalized * _movePower;
-        vel.y = _rb.velocity.y;
-        _rb.velocity = vel;
+        
     }
 }
