01b99cd3724506a4df834587390a0ec2ef726eeb
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-06-01 20:21:55 +0900

    PlayerController作成中

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -9,8 +9,8 @@ using UnityEngine.UI;
 /// </summary>
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
-    [Header("マウスカーソルをゲーム中に消すかどうかの設定")]
-    [SerializeField] bool _hideSystemMouseCursor = false;
+    //[Header("マウスカーソルをゲーム中に消すかどうかの設定")]
+    //[SerializeField] bool _hideSystemMouseCursor = false;
 
     [Header("敵関連")]
     [SerializeField, Tooltip("敵がいるレイヤー")] LayerMask _enemyLayer = default;
@@ -20,71 +20,71 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     [SerializeField, Tooltip("コイン数を表示させるテキスト")] Text _coinText;
     [Tooltip("所持している総コイン数")] [SerializeField] long _coin = 0;
 
-    [Header("照準関係")]
-    [SerializeField, Tooltip("照準のUI")] Image _crosshairImage;
-    [SerializeField, Tooltip("銃のオブジェクト")] GameObject _gunObject;
-    [SerializeField, Tooltip("銃から飛ばされるRayの距離")] float _rayRange = 100f;
+    //[Header("照準関係")]
+    //[SerializeField, Tooltip("照準のUI")] Image _crosshairImage;
+    //[SerializeField, Tooltip("銃のオブジェクト")] GameObject _gunObject;
+    //[SerializeField, Tooltip("銃から飛ばされるRayの距離")] float _rayRange = 100f;
 
-    [Header("現在所持している武器(初期装備はハンドガン)")]
-    [SerializeField] GameObject _initialWepon = default;
+    //[Header("現在所持している武器(初期装備はハンドガン)")]
+    //[SerializeField] GameObject _initialWepon = default;
 
     [Header("ゲームスタート時に呼ばれる")]
     [SerializeField, Tooltip("ゲームスタート時に呼び出す処理")] UnityEngine.Events.UnityEvent _onGameStart = null;
 
-    public void StartGame() //ゲームプレイを初期化する
-    {
-        Cursor.visible = _hideSystemMouseCursor;
+    //public void StartGame() //ゲームプレイを初期化する
+    //{
+    //    Cursor.visible = _hideSystemMouseCursor;
 
-        if (_hideSystemMouseCursor)
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        //Cursor.visible = _hideSystemMouseCursor;
-    }
+    //    if (_hideSystemMouseCursor)
+    //    {
+    //        Cursor.lockState = CursorLockMode.None;
+    //    }
+    //    else
+    //    {
+    //        Cursor.lockState = CursorLockMode.Locked;
+    //    }
+    //    //Cursor.visible = _hideSystemMouseCursor;
+    //}
 
     void Start()
     {
-        StartGame();
+        //StartGame();
     }
 
     void Update()
     {
-        // 照準の処理
-        _crosshairImage.rectTransform.position = Input.mousePosition;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        //// 照準の処理
+        //_crosshairImage.rectTransform.position = Input.mousePosition;
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
 
         //// 照準に敵がいるかどうかを調べる(Shot関数に書いたから要らないかも)
         //bool isEnemyTargeted = Physics.Raycast(ray, out hit, _rayRange, _enemyLayer);
         //_currentTarget = isEnemyTargeted ? hit.collider.gameObject.GetComponent<EnemyController>() : null;
 
-        // 銃の向く方向を変える
-        _gunObject.transform.rotation = Quaternion.LookRotation(ray.direction);
+        //// 銃の向く方向を変える
+        //_gunObject.transform.rotation = Quaternion.LookRotation(ray.direction);
 
-        if (Physics.Raycast(ray, out hit, 1f, LayerMask.GetMask("Gun")))
-        {
-            Cursor.visible = false;
-        }
-        else
-        {
-            Cursor.visible = true;
-        }
+        //if (Physics.Raycast(ray, out hit, 1f, LayerMask.GetMask("Gun")))
+        //{
+        //    Cursor.visible = false;
+        //}
+        //else
+        //{
+        //    Cursor.visible = true;
+        //}
 
-        // 左クリック(Shot)をした時
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shot();
-        }
+        //// 左クリック(Shot)をした時
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    Shot();
+        //}
         
-        //右クリック(AltShot)をした時
-        if (Input.GetButtonDown("Fire2"))
-        {
-            //AltShot();
-        }
+        ////右クリック(AltShot)をした時
+        //if (Input.GetButtonDown("Fire2"))
+        //{
+        //    //AltShot();
+        //}
     }
 
     public void Shot()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
-/// プレイヤーの動きを制御するコンポーネント(今んとこ使ってない、何かで使う可能性があるので残してる)
+/// プレイヤーの動きを制御するコンポーネント(銃とか照準とか)
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
-    void Start()
+    [Header("マウスカーソルをゲーム中に消すかどうかの設定")]
+    [SerializeField] bool _hideSystemMouseCursor = false;
+
+    [Header("照準関係")]
+    [SerializeField, Tooltip("照準のUI")] Image _crosshairImage;
+    [SerializeField, Tooltip("銃のオブジェクト")] GameObject _gunObject;
+    [SerializeField, Tooltip("銃から飛ばされるRayの距離")] float _rayRange = 100f;
+
+    [Header("現在装備中の武器")]
+    [SerializeField, Tooltip("現在装備中の武器")] GameObject _currentWepon = default;
+
+    /// <summary>
+    /// ゲームスタート時に呼ばれる
+    /// </summary>
+    void StartGame()
+    {
+        Cursor.visible = _hideSystemMouseCursor;
+
+        if (_hideSystemMouseCursor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void Update()
     {
         
     }
 
-    void Update()
+    void Crosshair()
     {
-        
+        // 照準の処理
+        _crosshairImage.rectTransform.position = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // 銃の向く方向を変える
+        _gunObject.transform.rotation = Quaternion.LookRotation(ray.direction);
+
+        if (Physics.Raycast(ray, out hit, 1f, LayerMask.GetMask("Gun")))
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+
+        // 左クリック(Shot)をした時
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }
+
+        //右クリック(AltShot)をした時
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //AltShot();
+        }
     }
 }
