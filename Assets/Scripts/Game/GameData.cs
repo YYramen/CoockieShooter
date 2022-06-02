924d0a024317680da658dd7a54d42e787a7747ce
YUSUKE <yoshidayusuke1124@gmail.com>
2022-06-02 22:28:08 +0900

    GameData.csのWeponTypeを増やした

--- Assets/Scripts/Game/GameData.cs
+++ Assets/Scripts/Game/GameData.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 
 
 /// <summary>
-/// 参照用のクラス。何かに保存したりはしない。
+/// ゲームに使うデータのクラス。保存用ではない。
 /// </summary>
 public enum ItemType
 {
-    Gun,
+    Wepon,
     Upgrade
 }
 
-public enum GunType
+public enum WeponType
 {
     HandGun,
     MachinePistol,
     ShotGun,
-    AssaultRifle
+    AssaultRifle,
+    Turret
 }
 
 [Serializable]
@@ -32,14 +33,16 @@ public class GameData
 {
     static public List<ItemTable> ItemTable = new List<ItemTable>()
     {
-        new ItemTable(){ Type = ItemType.Gun, Name = "ハンドガン", value = 10, targetId = 1},
-        new ItemTable(){ Type = ItemType.Gun, Name = "マシンピストル", value = 100, targetId = 2},
-        new ItemTable(){ Type = ItemType.Gun, Name = "ショットガン", value = 1000, targetId = 3},
-        new ItemTable(){ Type = ItemType.Gun, Name = "アサルトライフル", value = 10000, targetId = 4},
+        new ItemTable(){ Type = ItemType.Wepon, Name = "ハンドガン", value = 10, targetId = 1},
+        new ItemTable(){ Type = ItemType.Wepon, Name = "マシンピストル", value = 100, targetId = 2},
+        new ItemTable(){ Type = ItemType.Wepon, Name = "ショットガン", value = 1000, targetId = 3},
+        new ItemTable(){ Type = ItemType.Wepon, Name = "アサルトライフル", value = 10000, targetId = 4},
+        new ItemTable(){ Type = ItemType.Wepon, Name = "タレット", value = 1000, targetId = 5},
         new ItemTable(){ Type = ItemType.Upgrade, Name = "ハンドガンの威力2倍", value = 10, targetId = 1},
         new ItemTable(){ Type = ItemType.Upgrade, Name = "マシンピストルの威力2倍", value = 1000, targetId = 2},
         new ItemTable(){ Type = ItemType.Upgrade, Name = "ショットガンの威力2倍", value = 1000, targetId =3},
         new ItemTable(){ Type = ItemType.Upgrade, Name = "アサルトライフルの威力2倍", value = 10000, targetId = 4},
-        new ItemTable(){ Type = ItemType.Upgrade, Name = "全ての武器の威力2倍", value = 25000, targetId = 5}
+        new ItemTable(){ Type = ItemType.Upgrade, Name = "タレットの威力2倍", value = 1000, targetId = 5},
+        new ItemTable(){ Type = ItemType.Upgrade, Name = "全ての武器の威力2倍", value = 25000, targetId = 6}
     };
 }
